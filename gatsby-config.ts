4a77fd52a787408755b5453132507b7908959bd7
Kacper Drewniak <k.drewniak.dev@gmail.com>
2024-05-06 13:39:59 +0200

    fix gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,32 +5,9 @@ const config: GatsbyConfig = {
     title: 'tofu-gatsby-boilerplate',
     siteUrl: 'https://www.yourdomain.tld',
   },
-  graphqlTypegen: true,
   plugins: [
     'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: './src/assets/tofu.png',
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-favicons',
-      options: {
-        logo: './src/assets/tofu.png',
-        appName: 'TofuApp',
-        background: '#fff',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          yandex: false,
-          windows: false,
-        },
-      },
-    },
+
   ],
 };
 
