4319f31446a33c66fc5d06c11e63240dc2fcf873
Kacper Drewniak <k.drewniak.dev@gmail.com>
2024-06-08 10:30:34 +0200

    fix timer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,32 +9,8 @@ import { HeroImg, Logo } from '../../assets';
 import { Navbar } from '../Navbar/Navbar';
 
 export const Header = () => {
-  const [count, setCount] = useState<{
-    days:number;
-    hours:number;
-    minutes:number;
-    seconds:number;
-  }>(0);
-  const targetDate = moment('2024-06-08');
-  useEffect(() => {
-    // Implementing the setInterval method
-    const interval = setInterval(() => {
-      const now = moment();
-      const diff = targetDate.diff(now, 'seconds'); // Oblicz różnicę w sekundach.
-      const days = Math.floor(diff / 86400); // Oblicz liczbę dni.
-      const hours = Math.floor((diff % 86400) / 3600); // Oblicz liczbę godzin.
-      const minutes = Math.floor((diff % 3600) / 60); // Oblicz liczbę minut.
-      const seconds = diff % 60; // Oblicz liczbę sekund.
-      setCount({
-        days,
-        hours,
-        minutes,
-        seconds,
-      });
-    }, 1000);
 
-    return () => clearInterval(interval);
-  }, [count]);
+
 
   return (
     <HeaderStyled img={HeroImg as string}>
@@ -51,7 +27,7 @@ export const Header = () => {
         <LogoFloat src={Logo as string} alt="logo" />
         <TimeContainer>
           <Count>
-            {count.days}
+            0
           </Count>
           <TimeLabel>
             Dni
@@ -59,7 +35,7 @@ export const Header = () => {
         </TimeContainer>
         <TimeContainer>
           <Count>
-            {count.hours}
+            0
           </Count>
           <TimeLabel>
             Godz.
@@ -67,7 +43,7 @@ export const Header = () => {
         </TimeContainer>
         <TimeContainer>
           <Count>
-            {count.minutes}
+            0
           </Count>
           <TimeLabel>
             Min.
@@ -75,7 +51,7 @@ export const Header = () => {
         </TimeContainer>
         <TimeContainer>
           <Count>
-            {count.seconds}
+            0
           </Count>
           <TimeLabel>
             Sec.
